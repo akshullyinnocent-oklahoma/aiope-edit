e5f7c1ad446fd1a202ce793b7908242395c7a4cc
akshullyinnocent-oklahoma <akshullyinnocent@gmail.com>
2026-07-06 22:47:05 -0500

    Add Develocity configuration for build scans

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+        publishing.onlyIf { false }
+    }
+}
+
 buildscript {
   repositories {
     google()
